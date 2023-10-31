44777797a4893503473d81e47667d95d359ce2d3
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-11-01 03:44:44 +0900

    デバッグログ用に便利なメソッドを追加

--- kotlin-plugin/src/main/kotlin/com/github/kitakkun/back_in_time/MessageCollectorHolder.kt
+++ kotlin-plugin/src/main/kotlin/com/github/kitakkun/back_in_time/MessageCollectorHolder.kt
@@ -1,7 +1,16 @@
 package com.github.kitakkun.back_in_time
 
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 
 object MessageCollectorHolder {
     lateinit var messageCollector: MessageCollector
+
+    fun reportWarning(message: String) {
+        messageCollector.report(CompilerMessageSeverity.WARNING, message)
+    }
+
+    fun reportError(message: String) {
+        messageCollector.report(CompilerMessageSeverity.ERROR, message)
+    }
 }
