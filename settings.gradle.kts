c83aaf4814fb46a9424fbe93c3c55a2185ffb79c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-11-03 21:49:09 +0900

    Revert "kotlinを 1.9.20 に更新"
    This reverts commit 030762190b581996f4aac474b843e73df7a3e6c9.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,10 +5,10 @@ pluginManagement {
         google()
     }
     plugins {
-        kotlin("jvm") version "1.9.20" apply false
-        kotlin("kapt") version "1.9.20" apply false
+        kotlin("jvm") version "1.9.0" apply false
+        kotlin("kapt") version "1.9.0" apply false
         id("com.android.library") version "8.1.0" apply false
-        kotlin("multiplatform") version "1.9.20" apply false
+        kotlin("multiplatform") version "1.9.0" apply false
     }
 }
 
