02be64f56c3e9eeb59886672e63c3b642cab1b04
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-11-21 16:42:19 +0900

    未使用なクラスID削除

--- kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/BackInTimeConsts.kt
+++ kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/BackInTimeConsts.kt
@@ -15,7 +15,6 @@ object BackInTimeConsts {
     val backInTimeDebugServiceClassId = classId("com.github.kitakkun.backintime.runtime", "BackInTimeDebugService")
     val notifyPropertyChanged = "notifyPropertyChanged"
     val registerFunctionName = "register"
-    val systemClassId = classId("java.lang", "System")
     val printlnCallableId = CallableId(FqName("kotlin.io"), Name.identifier("println"))
 
     val methodCallInfoClassId = classId("com.github.kitakkun.backintime.runtime", "BackInTimeParentMethodCallInfo")
