0ba65edc1bf7595a47459c942d8460b642eb7dbc
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-11-21 20:54:20 +0900

    デモのコードを簡素化

--- demo/src/main/kotlin/com/github/kitakkun/back_in_time/HogeViewModel.kt
+++ demo/src/main/kotlin/com/github/kitakkun/back_in_time/HogeViewModel.kt
@@ -3,37 +3,17 @@ package com.github.kitakkun.back_in_time
 import com.github.kitakkun.backintime.annotations.DebuggableStateHolder
 
 @DebuggableStateHolder
-class HogeViewModel(
-    private val aa: String,
-) {
-    var hoge = ""
-    var fuga = 0
-    var foo = 20.0
+class HogeViewModel {
     val valueContainer = ValueContainer("hoge")
 
-    constructor() : this("hogehoge")
-
     fun set() {
-//        val a = java.util.UUID.randomUUID().toString()
-//        hoge = "hoge"
-//        fuga = 100
-//        foo = 200.0
-        valueContainer.value = "fuga"
-        valueContainer.postValue("HOHO")
-    }
-
-    fun hogehoge(a: Int = 10): Int {
-//        hoge += hoge
-        return a
+        valueContainer.value = "foo"
+        valueContainer.postValue("bar")
     }
 }
 
 
 class ValueContainer<T>(var value: T) {
-    fun set(value: T) {
-        this.value = value
-    }
-
     fun postValue(value: T) {
         this.value = value
     }
