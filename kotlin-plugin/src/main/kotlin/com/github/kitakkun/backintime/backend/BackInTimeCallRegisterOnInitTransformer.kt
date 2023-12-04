d3d54f0ea9f3fe12c110d3ee0ae1eb9aae465808
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-12-04 15:41:42 +0900

    リファクタ

--- kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/backend/BackInTimeCallRegisterOnInitTransformer.kt
+++ kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/backend/BackInTimeCallRegisterOnInitTransformer.kt
@@ -4,7 +4,9 @@ import com.github.kitakkun.backintime.BackInTimeConsts
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.builders.*
+import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrConstructor
+import org.jetbrains.kotlin.ir.declarations.IrProperty
 import org.jetbrains.kotlin.ir.types.classFqName
 import org.jetbrains.kotlin.ir.types.defaultType
 import org.jetbrains.kotlin.ir.util.*
@@ -33,32 +35,45 @@ class BackInTimeCallRegisterOnInitTransformer(
             scope = Scope(declaration.symbol),
         ).blockBody {
             +declaration.body?.statements.orEmpty()
-            +irCall(registerFunction).apply {
-                dispatchReceiver = irGetObject(backInTimeDebugServiceClass)
-                putValueArgument(0, irGet(parentClass.thisReceiver!!))
-                putValueArgument(1, irCallConstructor(instanceInfoConstructor, emptyList()).apply {
-                    putValueArgument(0, irString(parentClass.fqNameWhenAvailable?.asString() ?: return@blockBody))
-                    putValueArgument(1, irCall(listOfFunction).apply {
-                        putValueArgument(0, irVararg(propertyInfoClass.defaultType, parentClass.properties.map { irProperty ->
-                            irCallConstructor(propertyInfoClassConstructor, emptyList()).apply {
-                                putValueArgument(0, irString(irProperty.name.asString()))
-                                putValueArgument(1, irBoolean(true)) // FIXME: 適当に入れてる
-                                val genericTypes = irProperty.getGenericTypes()
-                                if (genericTypes.isEmpty()) {
-                                    putValueArgument(2, irString(irProperty.backingField?.type?.classFqName?.asString() ?: "unknown"))
-                                    putValueArgument(3, irString(irProperty.backingField?.type?.classFqName?.asString() ?: "unknown"))
-                                } else {
-                                    putValueArgument(2, irString(irProperty.backingField?.type?.classFqName?.asString() ?: "unknown"))
-                                    putValueArgument(3, irString(genericTypes.first().classFqName?.asString() ?: "unknown"))
-                                }
-                            }
-                        }.toList()))
-                        putTypeArgument(0, propertyInfoClass.defaultType)
-                    })
-                })
-            }
+            +generateRegisterCall(parentClass)
         }
 
         return super.visitConstructor(declaration)
+    }
+
+    // BackInTimeDebugService.register(this, InstanceInfo(...))
+    private fun IrBuilderWithScope.generateRegisterCall(parentClass: IrClass) = irCall(registerFunction).apply {
+        dispatchReceiver = irGetObject(backInTimeDebugServiceClass)
+        putValueArgument(0, irGet(parentClass.thisReceiver!!))
+        putValueArgument(1, generateInstanceInfo(parentClass))
+    }
+
+    // InstanceInfo(parentClass.fqNameWhenAvailable, listOf(PropertyInfo(...)))
+    private fun IrBuilderWithScope.generateInstanceInfo(
+        parentClass: IrClass,
+    ) = irCallConstructor(instanceInfoConstructor, emptyList()).apply {
+        putValueArgument(0, irString(parentClass.fqNameWhenAvailable?.asString() ?: "unknown"))
+        putValueArgument(1, generatePropertiesInfo(parentClass.properties))
+    }
+
+    // listOf(PropertyInfo(...), PropertyInfo(...), ...)
+    private fun IrBuilderWithScope.generatePropertiesInfo(
+        properties: Sequence<IrProperty>,
+    ) = irCall(listOfFunction).apply {
+        putValueArgument(0, irVararg(propertyInfoClass.defaultType, properties.map { irProperty ->
+            irCallConstructor(propertyInfoClassConstructor, emptyList()).apply {
+                putValueArgument(0, irString(irProperty.name.asString()))
+                putValueArgument(1, irBoolean(true)) // FIXME: 適当に入れてる
+                val genericTypes = irProperty.getGenericTypes()
+                if (genericTypes.isEmpty()) {
+                    putValueArgument(2, irString(irProperty.backingField?.type?.classFqName?.asString() ?: "unknown"))
+                    putValueArgument(3, irString(irProperty.backingField?.type?.classFqName?.asString() ?: "unknown"))
+                } else {
+                    putValueArgument(2, irString(irProperty.backingField?.type?.classFqName?.asString() ?: "unknown"))
+                    putValueArgument(3, irString(genericTypes.first().classFqName?.asString() ?: "unknown"))
+                }
+            }
+        }.toList()))
+        putTypeArgument(0, propertyInfoClass.defaultType)
     }
 }
