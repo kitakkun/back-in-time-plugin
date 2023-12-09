fe93e821d6a900db6f33787b53a5f2f2e5246560
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-12-09 23:39:49 +0900

    テストの修正

--- back-in-time.library/src/jvmTest/kotlin/com/github/kitakkun/backintime/BackInTimeDebugServiceTest.kt
+++ back-in-time.library/src/jvmTest/kotlin/com/github/kitakkun/backintime/BackInTimeDebugServiceTest.kt
@@ -1,18 +1,32 @@
 package com.github.kitakkun.backintime
 
 import com.github.kitakkun.backintime.runtime.BackInTimeDebugService
+import com.github.kitakkun.backintime.runtime.DebuggableStateHolderManipulator
+import com.github.kitakkun.backintime.runtime.InstanceInfo
 import org.junit.Test
 import java.lang.Thread.sleep
 import kotlin.test.assertEquals
 
 class BackInTimeDebugServiceTest {
-    class TestClass
+    class TestClass : DebuggableStateHolderManipulator {
+        override fun deserializePropertyValueForBackInTimeDebug(propertyName: String, value: String): Any? {
+            TODO("Not yet implemented")
+        }
+
+        override fun forceSetPropertyValueForBackInTimeDebug(propertyName: String, value: Any?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun serializePropertyValueForBackInTimeDebug(propertyName: String, value: Any?): String {
+            TODO("Not yet implemented")
+        }
+    }
 
     @Test
     fun testIfInstanceAutomaticallyUnregistered() {
         val service = BackInTimeDebugService
         var instance: TestClass? = TestClass()
-        service.register(instance!!)
+        service.register(instance!!, InstanceInfo("test", emptyList(), "test"))
         assertEquals(expected = 1, actual = service.instances.size)
         instance = null
         System.gc()
