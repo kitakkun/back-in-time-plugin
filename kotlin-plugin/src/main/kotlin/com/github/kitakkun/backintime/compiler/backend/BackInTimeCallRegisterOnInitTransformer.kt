10973e1b034f7421221603505c104c6379ea8927
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-12-10 12:01:21 +0900

    fix the references of wrong constructors

--- kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/backend/BackInTimeCallRegisterOnInitTransformer.kt
+++ kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/backend/BackInTimeCallRegisterOnInitTransformer.kt
@@ -3,13 +3,28 @@ package com.github.kitakkun.backintime.compiler.backend
 import com.github.kitakkun.backintime.compiler.BackInTimeConsts
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.IrStatement
-import org.jetbrains.kotlin.ir.builders.*
+import org.jetbrains.kotlin.ir.builders.IrBlockBodyBuilder
+import org.jetbrains.kotlin.ir.builders.IrBuilderWithScope
+import org.jetbrains.kotlin.ir.builders.Scope
+import org.jetbrains.kotlin.ir.builders.irBoolean
+import org.jetbrains.kotlin.ir.builders.irCall
+import org.jetbrains.kotlin.ir.builders.irCallConstructor
+import org.jetbrains.kotlin.ir.builders.irGet
+import org.jetbrains.kotlin.ir.builders.irGetObject
+import org.jetbrains.kotlin.ir.builders.irString
+import org.jetbrains.kotlin.ir.builders.irVararg
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrConstructor
 import org.jetbrains.kotlin.ir.declarations.IrProperty
 import org.jetbrains.kotlin.ir.types.classFqName
 import org.jetbrains.kotlin.ir.types.defaultType
-import org.jetbrains.kotlin.ir.util.*
+import org.jetbrains.kotlin.ir.util.constructors
+import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
+import org.jetbrains.kotlin.ir.util.getSimpleFunction
+import org.jetbrains.kotlin.ir.util.isVararg
+import org.jetbrains.kotlin.ir.util.parentClassOrNull
+import org.jetbrains.kotlin.ir.util.properties
+import org.jetbrains.kotlin.ir.util.statements
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 
 class BackInTimeCallRegisterOnInitTransformer(
@@ -17,9 +32,11 @@ class BackInTimeCallRegisterOnInitTransformer(
 ) : IrElementTransformerVoid() {
     private val backInTimeDebugServiceClass = pluginContext.referenceClass(BackInTimeConsts.backInTimeDebugServiceClassId)!!
     private val registerFunction = backInTimeDebugServiceClass.getSimpleFunction(BackInTimeConsts.registerFunctionName)!!
-    private val instanceInfoConstructor = pluginContext.referenceConstructors(BackInTimeConsts.instanceInfoClassId).first()
+
+    // find by isPrimary because kotlinx-serialization generates secondary constructor
+    private val instanceInfoConstructor = pluginContext.referenceConstructors(BackInTimeConsts.instanceInfoClassId).first { it.owner.isPrimary }
     private val propertyInfoClass = pluginContext.referenceClass(BackInTimeConsts.propertyInfoClassId)!!
-    private val propertyInfoClassConstructor = propertyInfoClass.constructors.first()
+    private val propertyInfoClassConstructor = propertyInfoClass.constructors.first { it.owner.isPrimary }
     private val listOfFunction = pluginContext.referenceFunctions(BackInTimeConsts.listOfFunctionId).first {
         it.owner.valueParameters.size == 1 && it.owner.valueParameters.first().isVararg
     }
