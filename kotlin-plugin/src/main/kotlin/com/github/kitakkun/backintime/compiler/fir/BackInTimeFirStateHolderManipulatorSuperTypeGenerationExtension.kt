41165df5e051c2c6e6335d4661fc777beb28abe1
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-12-13 02:46:24 +0900

    robolectricのテストでFIRエラーが出る問題の修正

--- kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/fir/BackInTimeFirStateHolderManipulatorSuperTypeGenerationExtension.kt
+++ kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/fir/BackInTimeFirStateHolderManipulatorSuperTypeGenerationExtension.kt
@@ -4,6 +4,7 @@ import com.github.kitakkun.backintime.compiler.BackInTimeAnnotations
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirClassLikeDeclaration
 import org.jetbrains.kotlin.fir.declarations.hasAnnotation
+import org.jetbrains.kotlin.fir.declarations.hasAnnotationSafe
 import org.jetbrains.kotlin.fir.extensions.FirSupertypeGenerationExtension
 import org.jetbrains.kotlin.fir.types.FirResolvedTypeRef
 import org.jetbrains.kotlin.fir.types.builder.buildResolvedTypeRef
@@ -12,7 +13,7 @@ import org.jetbrains.kotlin.fir.types.toLookupTag
 
 class BackInTimeFirStateHolderManipulatorSuperTypeGenerationExtension(session: FirSession) : FirSupertypeGenerationExtension(session = session) {
     override fun needTransformSupertypes(declaration: FirClassLikeDeclaration): Boolean {
-        return declaration.hasAnnotation(BackInTimeAnnotations.debuggableStateHolderAnnotationClassId, session)
+        return declaration.hasAnnotationSafe(BackInTimeAnnotations.debuggableStateHolderAnnotationClassId, session)
     }
 
     context(TypeResolveServiceContainer) override fun computeAdditionalSupertypes(classLikeDeclaration: FirClassLikeDeclaration, resolvedSupertypes: List<FirResolvedTypeRef>): List<FirResolvedTypeRef> {
