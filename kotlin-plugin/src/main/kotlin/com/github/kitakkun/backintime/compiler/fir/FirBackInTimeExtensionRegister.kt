55a47b7881ad3e77273535f7a7be740daf8bd316
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-12-15 17:21:59 +0900

    checkerが正しく動いてなさそうなので一旦無効にする

--- kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/fir/FirBackInTimeExtensionRegister.kt
+++ kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/fir/FirBackInTimeExtensionRegister.kt
@@ -1,12 +1,10 @@
 package com.github.kitakkun.backintime.compiler.fir
 
-import com.github.kitakkun.backintime.compiler.fir.checkers.BackInTimeFirAdditionalCheckersExtension
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 
 class BackInTimeFirExtensionRegistrar : FirExtensionRegistrar() {
     override fun ExtensionRegistrarContext.configurePlugin() {
         +::BackInTimeFirStateHolderManipulatorSuperTypeGenerationExtension
         +::FirManipulatorMethodsDeclarationGenerationExtension
-        +::BackInTimeFirAdditionalCheckersExtension
     }
 }
