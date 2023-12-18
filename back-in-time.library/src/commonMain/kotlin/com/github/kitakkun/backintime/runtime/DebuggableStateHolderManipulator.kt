40a034d5c02a7f6dd88c0f7a8ad10dca6874072b
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-12-19 02:31:13 +0900

    発生する可能性のあるExceptionを明記する

--- back-in-time.library/src/commonMain/kotlin/com/github/kitakkun/backintime/runtime/DebuggableStateHolderManipulator.kt
+++ back-in-time.library/src/commonMain/kotlin/com/github/kitakkun/backintime/runtime/DebuggableStateHolderManipulator.kt
@@ -1,12 +1,15 @@
 package com.github.kitakkun.backintime.runtime
 
 import com.github.kitakkun.backintime.runtime.exception.BackInTimeRuntimeException
+import kotlinx.serialization.SerializationException
 
 interface DebuggableStateHolderManipulator {
     @Throws(BackInTimeRuntimeException.NullValueNotAssignableException::class, BackInTimeRuntimeException.NoSuchPropertyException::class)
     fun forceSetValue(propertyName: String, value: Any?)
 
+    @Throws(BackInTimeRuntimeException.NoSuchPropertyException::class, SerializationException::class)
     fun serializeValue(propertyName: String, value: Any?): String
 
+    @Throws(BackInTimeRuntimeException.NoSuchPropertyException::class, SerializationException::class)
     fun deserializeValue(propertyName: String, value: String): Any?
 }
