15dea7aa26d96877b3684967742d25b6e87b44b2
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-12-30 00:44:25 +0900

    ValueContainerInfoの解決をBackInTimePluginContextに移す

--- kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/backend/BackInTimeIrGenerationExtension.kt
+++ kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/backend/BackInTimeIrGenerationExtension.kt
@@ -4,49 +4,17 @@ import com.github.kitakkun.backintime.compiler.BackInTimeCompilerConfiguration
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
-import org.jetbrains.kotlin.name.CallableId
 
 class BackInTimeIrGenerationExtension(
     private val config: BackInTimeCompilerConfiguration,
 ) : IrGenerationExtension {
     override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
-        val valueContainerInfo = with(UserDefinedValueContainerAnalyzer()) {
-            moduleFragment.acceptChildrenVoid(this)
-            resolveIdsToValueContainerInfoList(
-                capturedCallableIds = config.capturedCallableIds + collectedCapturedCallableIds,
-                valueGetterCallableIds = config.valueGetterCallableIds + collectedGetterCallableIds,
-                valueSetterCallableIds = config.valueSetterCallableIds + collectedSetterCallableIds,
-            )
-        }
-
-        val context = BackInTimePluginContext(
-            baseContext = pluginContext,
-            valueContainerClassInfoList = valueContainerInfo,
-        )
-
+        val context = BackInTimePluginContext(baseContext = pluginContext, config = config, moduleFragment = moduleFragment)
         with(context) {
             moduleFragment.transformChildrenVoid(BackInTimeCallRegisterOnInitTransformer())
             moduleFragment.transformChildrenVoid(BackInTimeIrValueChangeNotifyCodeGenerationExtension())
             moduleFragment.transformChildrenVoid(GenerateManipulatorMethodBodyTransformer())
         }
-    }
-
-    private fun resolveIdsToValueContainerInfoList(
-        capturedCallableIds: Set<CallableId>,
-        valueGetterCallableIds: Set<CallableId>,
-        valueSetterCallableIds: Set<CallableId>,
-    ): List<ValueContainerClassInfo> {
-        return capturedCallableIds
-            .mapNotNull { it.classId }
-            .mapNotNull { classId ->
-                ValueContainerClassInfo(
-                    classId = classId,
-                    capturedCallableIds = capturedCallableIds.filter { it.classId == classId },
-                    valueGetter = valueGetterCallableIds.firstOrNull { it.classId == classId } ?: return@mapNotNull null,
-                    valueSetter = valueSetterCallableIds.firstOrNull { it.classId == classId } ?: return@mapNotNull null,
-                )
-            }
     }
 }
--- kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/backend/BackInTimePluginContext.kt
+++ kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/backend/BackInTimePluginContext.kt
@@ -1,19 +1,32 @@
 package com.github.kitakkun.backintime.compiler.backend
 
+import com.github.kitakkun.backintime.compiler.BackInTimeCompilerConfiguration
 import com.github.kitakkun.backintime.compiler.BackInTimeConsts
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.jvm.ir.isReifiable
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.types.defaultType
 import org.jetbrains.kotlin.ir.util.classId
 import org.jetbrains.kotlin.ir.util.constructors
 import org.jetbrains.kotlin.ir.util.getSimpleFunction
 import org.jetbrains.kotlin.ir.util.isVararg
+import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
+import org.jetbrains.kotlin.name.CallableId
 
 class BackInTimePluginContext(
     baseContext: IrPluginContext,
-    val valueContainerClassInfoList: List<ValueContainerClassInfo>,
+    config: BackInTimeCompilerConfiguration,
+    moduleFragment: IrModuleFragment,
 ) : IrPluginContext by baseContext {
     val pluginContext: IrPluginContext = baseContext
+    val valueContainerClassInfoList: List<ValueContainerClassInfo> = with(UserDefinedValueContainerAnalyzer()) {
+        moduleFragment.acceptChildrenVoid(this)
+        resolveIdsToValueContainerInfoList(
+            capturedCallableIds = config.capturedCallableIds + collectedCapturedCallableIds,
+            valueGetterCallableIds = config.valueGetterCallableIds + collectedGetterCallableIds,
+            valueSetterCallableIds = config.valueSetterCallableIds + collectedSetterCallableIds,
+        )
+    }
 
     // BackInTimeService
     val backInTimeServiceClassSymbol = referenceClass(BackInTimeConsts.backInTimeDebugServiceClassId)!!
@@ -48,4 +61,20 @@ class BackInTimePluginContext(
         it.owner.isReifiable() && it.owner.typeParameters.size == 1 && it.owner.valueParameters.size == 1
     } ?: error("${BackInTimeConsts.kotlinxSerializationDecodeFromStringCallableId} is not found. Make sure you have kotlinx-serialization runtime dependency.")
 
+    private fun resolveIdsToValueContainerInfoList(
+        capturedCallableIds: Set<CallableId>,
+        valueGetterCallableIds: Set<CallableId>,
+        valueSetterCallableIds: Set<CallableId>,
+    ): List<ValueContainerClassInfo> {
+        return capturedCallableIds
+            .mapNotNull { it.classId }
+            .mapNotNull { classId ->
+                ValueContainerClassInfo(
+                    classId = classId,
+                    capturedCallableIds = capturedCallableIds.filter { it.classId == classId },
+                    valueGetter = valueGetterCallableIds.firstOrNull { it.classId == classId } ?: return@mapNotNull null,
+                    valueSetter = valueSetterCallableIds.firstOrNull { it.classId == classId } ?: return@mapNotNull null,
+                )
+            }
+    }
 }
