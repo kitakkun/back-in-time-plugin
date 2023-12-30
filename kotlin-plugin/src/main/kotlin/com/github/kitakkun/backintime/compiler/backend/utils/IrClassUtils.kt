509d6d6036e406614c402ac9d0f72c2a260a56d9
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-12-30 09:29:27 +0900

    再帰的なプロパティゲッターの取得と関数の取得をより正しいロジックに修正

--- kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/backend/utils/IrClassUtils.kt
+++ kotlin-plugin/src/main/kotlin/com/github/kitakkun/backintime/compiler/backend/utils/IrClassUtils.kt
@@ -5,17 +5,18 @@ import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
 import org.jetbrains.kotlin.ir.types.classOrNull
 import org.jetbrains.kotlin.ir.util.getPropertyGetter
 import org.jetbrains.kotlin.ir.util.getSimpleFunction
+import org.jetbrains.kotlin.synthetic.isVisibleOutside
 
 fun IrClass.getSimpleFunctionRecursively(name: String): IrSimpleFunctionSymbol? {
     return getSimpleFunction(name)
         ?: superTypes
-            .mapNotNull { it.classOrNull }
-            .firstNotNullOfOrNull { it.getSimpleFunction(name) }
+            .mapNotNull { it.classOrNull?.owner?.getSimpleFunctionRecursively(name) }
+            .firstOrNull { it.owner.visibility.isVisibleOutside() }
 }
 
 fun IrClass.getPropertyGetterRecursively(name: String): IrSimpleFunctionSymbol? {
     return getPropertyGetter(name)
         ?: superTypes
-            .mapNotNull { it.classOrNull }
-            .firstNotNullOfOrNull { it.getPropertyGetter(name) }
+            .mapNotNull { it.classOrNull?.owner?.getSimpleFunctionRecursively(name) }
+            .firstOrNull { it.owner.visibility.isVisibleOutside() }
 }
