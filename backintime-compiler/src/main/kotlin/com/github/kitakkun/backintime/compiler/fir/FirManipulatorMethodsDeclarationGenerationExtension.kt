0f537d2c419cb09793c35050ee8a9414c5c33ddf
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-01-07 21:03:03 +0900

    メソッドのオーバーライド生成をスーパータイプのシンボルを取得して自動生成

--- backintime-compiler/src/main/kotlin/com/github/kitakkun/backintime/compiler/fir/FirManipulatorMethodsDeclarationGenerationExtension.kt
+++ backintime-compiler/src/main/kotlin/com/github/kitakkun/backintime/compiler/fir/FirManipulatorMethodsDeclarationGenerationExtension.kt
@@ -1,66 +1,40 @@
 package com.github.kitakkun.backintime.compiler.fir
 
+import com.github.kitakkun.backintime.compiler.BackInTimeAnnotations
 import com.github.kitakkun.backintime.compiler.BackInTimeConsts
 import com.github.kitakkun.backintime.compiler.BackInTimePluginKey
 import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.analysis.checkers.toRegularClassSymbol
 import org.jetbrains.kotlin.fir.extensions.FirDeclarationGenerationExtension
 import org.jetbrains.kotlin.fir.extensions.MemberGenerationContext
 import org.jetbrains.kotlin.fir.plugin.createMemberFunction
 import org.jetbrains.kotlin.fir.symbols.impl.FirClassSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
-import org.jetbrains.kotlin.fir.types.coneType
+import org.jetbrains.kotlin.fir.types.classId
 import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.Name
 
 class FirManipulatorMethodsDeclarationGenerationExtension(session: FirSession) : FirDeclarationGenerationExtension(session) {
     override fun generateFunctions(callableId: CallableId, context: MemberGenerationContext?): List<FirNamedFunctionSymbol> {
         val ownerClass = context?.owner ?: return emptyList()
-        return when (callableId.callableName) {
-            BackInTimeConsts.serializeMethodName -> listOf(
-                createMemberFunction(
-                    owner = ownerClass,
-                    key = BackInTimePluginKey,
-                    name = callableId.callableName,
-                    returnType = session.builtinTypes.stringType.coneType,
-                    config = {
-                        valueParameter(Name.identifier("propertyName"), type = session.builtinTypes.stringType.coneType)
-                        valueParameter(Name.identifier("value"), type = session.builtinTypes.nullableAnyType.coneType)
-                        status { isOverride = true }
-                    },
-                ).symbol
-            )
-
-            BackInTimeConsts.forceSetValueMethodName -> listOf(
-                createMemberFunction(
-                    owner = ownerClass,
-                    key = BackInTimePluginKey,
-                    name = callableId.callableName,
-                    returnType = session.builtinTypes.unitType.coneType,
-                    config = {
-                        valueParameter(Name.identifier("propertyName"), type = session.builtinTypes.stringType.coneType)
-                        valueParameter(Name.identifier("value"), type = session.builtinTypes.nullableAnyType.coneType)
-                        status { isOverride = true }
-                    },
-                ).symbol
-            )
-
-            BackInTimeConsts.deserializeMethodName -> listOf(
-                createMemberFunction(
-                    owner = ownerClass,
-                    key = BackInTimePluginKey,
-                    name = callableId.callableName,
-                    returnType = session.builtinTypes.nullableAnyType.coneType,
-                    config = {
-                        valueParameter(Name.identifier("propertyName"), type = session.builtinTypes.stringType.coneType)
-                        valueParameter(Name.identifier("value"), type = session.builtinTypes.stringType.coneType)
-                        status { isOverride = true }
-                    },
-                ).symbol
-            )
-
-            else -> emptyList()
-        }
+        val manipulatorClassSymbol = ownerClass.resolvedSuperTypeRefs
+            .find { it.type.classId == BackInTimeAnnotations.debuggableStateHolderManipulatorAnnotationClassId }
+            ?.toRegularClassSymbol(session) ?: return emptyList()
+        val manipulatorFunctionSymbols = manipulatorClassSymbol.declarationSymbols.filterIsInstance<FirNamedFunctionSymbol>()
+        val correspondingFunctionSymbol = manipulatorFunctionSymbols.find { it.callableId.callableName == callableId.callableName } ?: return emptyList()
+        return listOf(
+            createMemberFunction(
+                owner = ownerClass,
+                key = BackInTimePluginKey,
+                name = callableId.callableName,
+                returnType = correspondingFunctionSymbol.resolvedReturnType,
+                config = {
+                    correspondingFunctionSymbol.valueParameterSymbols.forEach { valueParameter(it.name, it.resolvedReturnType) }
+                    status { isOverride = true }
+                }
+            ).symbol
+        )
     }
 
     override fun getCallableNamesForClass(classSymbol: FirClassSymbol<*>, context: MemberGenerationContext): Set<Name> {
