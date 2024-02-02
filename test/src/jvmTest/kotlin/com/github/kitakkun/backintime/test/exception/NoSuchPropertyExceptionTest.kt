6d403064e65c68d9a06cc144ecc9c3e50ae14bd7
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-02-02 13:07:28 +0900

    Exception関連のテストを追加

---
+++ test/src/jvmTest/kotlin/com/github/kitakkun/backintime/test/exception/NoSuchPropertyExceptionTest.kt
@@ -0,0 +1,42 @@
+package com.github.kitakkun.backintime.test.exception
+
+import com.github.kitakkun.backintime.annotations.DebuggableStateHolder
+import com.github.kitakkun.backintime.runtime.BackInTimeDebuggable
+import com.github.kitakkun.backintime.runtime.exception.BackInTimeRuntimeException
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertIs
+
+class NoSuchPropertyExceptionTest {
+    @DebuggableStateHolder
+    private class TestStateHolder {
+        val property1: String = "test"
+    }
+
+    @Test
+    fun testForceSetValue() {
+        val holder = TestStateHolder()
+        assertIs<BackInTimeDebuggable>(holder)
+        assertFailsWith(BackInTimeRuntimeException.NoSuchPropertyException::class) {
+            holder.forceSetValue("property2", "test")
+        }
+    }
+
+    @Test
+    fun testSerializeValue() {
+        val holder = TestStateHolder()
+        assertIs<BackInTimeDebuggable>(holder)
+        assertFailsWith(BackInTimeRuntimeException.NoSuchPropertyException::class) {
+            holder.serializeValue("property2", "test")
+        }
+    }
+
+    @Test
+    fun testDeserializeValue() {
+        val holder = TestStateHolder()
+        assertIs<BackInTimeDebuggable>(holder)
+        assertFailsWith(BackInTimeRuntimeException.NoSuchPropertyException::class) {
+            holder.deserializeValue("property2", "test")
+        }
+    }
+}
---
+++ test/src/jvmTest/kotlin/com/github/kitakkun/backintime/test/exception/TypeMismatchExceptionTest.kt
@@ -0,0 +1,41 @@
+package com.github.kitakkun.backintime.test.exception
+
+import com.github.kitakkun.backintime.annotations.Capture
+import com.github.kitakkun.backintime.annotations.DebuggableStateHolder
+import com.github.kitakkun.backintime.annotations.Getter
+import com.github.kitakkun.backintime.annotations.Setter
+import com.github.kitakkun.backintime.annotations.ValueContainer
+import com.github.kitakkun.backintime.runtime.BackInTimeDebuggable
+import com.github.kitakkun.backintime.runtime.exception.BackInTimeRuntimeException
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertIs
+
+class TypeMismatchExceptionTest {
+    @ValueContainer
+    private class AnnotationConfiguredValueContainer<T>(@Getter @Setter @Capture var value: T)
+
+    @DebuggableStateHolder
+    private class TestStateHolder {
+        val property: String = ""
+        val valueContainerProperty = AnnotationConfiguredValueContainer(0)
+    }
+
+    @Test
+    fun testNormalProperty() {
+        val holder = TestStateHolder()
+        assertIs<BackInTimeDebuggable>(holder)
+        assertFailsWith(BackInTimeRuntimeException.TypeMismatchException::class) {
+            holder.forceSetValue("property", 1)
+        }
+    }
+
+    @Test
+    fun testValueContainerProperty() {
+        val holder = TestStateHolder()
+        assertIs<BackInTimeDebuggable>(holder)
+        assertFailsWith(BackInTimeRuntimeException.TypeMismatchException::class) {
+            holder.forceSetValue("valueContainerProperty", "test")
+        }
+    }
+}
