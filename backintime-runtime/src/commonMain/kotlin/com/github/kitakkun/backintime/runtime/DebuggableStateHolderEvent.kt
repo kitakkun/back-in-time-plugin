e77871cbd58261dcc7c9c17c20df51020f2b3ca9
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-02-18 13:38:25 +0900

    remove unused property 'priority'

--- backintime-runtime/src/commonMain/kotlin/com/github/kitakkun/backintime/runtime/DebuggableStateHolderEvent.kt
+++ backintime-runtime/src/commonMain/kotlin/com/github/kitakkun/backintime/runtime/DebuggableStateHolderEvent.kt
@@ -1,25 +1,20 @@
 package com.github.kitakkun.backintime.runtime
 
 sealed interface DebuggableStateHolderEvent {
-    val priority: Int
-
     data class RegisterInstance(
         val instance: BackInTimeDebuggable,
         val info: InstanceInfo,
-        override val priority: Int = 0,
     ) : DebuggableStateHolderEvent
 
     data class RegisterRelationShip(
         val parentInstance: BackInTimeDebuggable,
         val childInstance: BackInTimeDebuggable,
-        override val priority: Int = 1,
     ) : DebuggableStateHolderEvent
 
     data class MethodCall(
         val instance: BackInTimeDebuggable,
         val methodCallId: String,
         val methodName: String,
-        override val priority: Int = 1,
     ) : DebuggableStateHolderEvent
 
     data class PropertyValueChange(
@@ -27,6 +22,5 @@ sealed interface DebuggableStateHolderEvent {
         val methodCallId: String,
         val propertyName: String,
         val propertyValue: Any?,
-        override val priority: Int = 2,
     ) : DebuggableStateHolderEvent
 }
