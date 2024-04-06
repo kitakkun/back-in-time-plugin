644285275b5fb72271333e766508fa5e6c962d92
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-04-07 00:36:46 +0900

    Revert "add needless blank lines to test danger comment"
    This reverts commit c9822b5133c91a6f41d68a18d398ca74e5a4bd34.

--- build-logic/convention/src/main/kotlin/com/github/kitakkun/backintime/convention/LintConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/github/kitakkun/backintime/convention/LintConventionPlugin.kt
@@ -9,8 +9,6 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 class LintConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-
-
         with(target) {
             with(pluginManager) {
                 apply("org.jlleitschuh.gradle.ktlint")
