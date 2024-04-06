a19974f5c7a1a8c3769a84fc81273c96b328a036
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-04-07 01:09:32 +0900

    configure data module

---
+++ backintime-debugger/data/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.sqldelight)
+    alias(libs.plugins.backintimeLint)
+}
+
+kotlin {
+    jvm()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":backintime-runtime"))
+            implementation(libs.sqldelight.sqlite.driver)
+            implementation(libs.sqldelight.coroutines.extensions)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.multiplatform.settings)
+            implementation(libs.koin.core)
+            implementation(libs.ktor.serialization.kotlinx.json)
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.kotlin.test.junit)
+            implementation(libs.koin.test)
+        }
+
+        jvmMain.dependencies {
+            implementation(libs.ktor.server.core.jvm)
+            implementation(libs.ktor.server.websockets.jvm)
+            implementation(libs.ktor.server.netty.jvm)
+        }
+    }
+}
+
+sqldelight {
+    databases {
+        create("BackInTimeDatabase") {
+            packageName.set("com.github.kitakkun.backintime.debugger.data")
+        }
+    }
+}
