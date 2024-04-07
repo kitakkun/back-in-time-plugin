5a51c407ef1e6c790fffe2ed002454cad9772059
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-04-07 10:51:26 +0900

    fix missing eventLogAdapter for test di module

--- backintime-debugger/data/src/commonTest/kotlin/com/github/kitakkun/backintime/debugger/repository/TestDataModule.kt
+++ backintime-debugger/data/src/commonTest/kotlin/com/github/kitakkun/backintime/debugger/repository/TestDataModule.kt
@@ -3,8 +3,10 @@ package com.github.kitakkun.backintime.debugger.repository
 import com.github.kitakkun.backintime.debugger.data.BackInTimeDatabase
 import com.github.kitakkun.backintime.debugger.data.repository.ClassInfoRepository
 import com.github.kitakkun.backintime.debugger.data.repository.ClassInfoRepositoryImpl
+import com.github.kitakkun.backintime.debugger.data.repository.backInTimeDebugServiceEventAdapter
 import com.github.kitakkun.backintime.debugger.data.repository.listOfPropertyInfoAdapter
 import com.github.kitakkun.backintime.debugger.database.ClassInfo
+import com.github.kitakkun.backintime.debugger.database.EventLog
 import org.koin.dsl.module
 
 val testDataModule = module {
@@ -17,6 +19,9 @@ val testDataModule = module {
             driver = driver,
             classInfoAdapter = ClassInfo.Adapter(
                 propertiesAdapter = listOfPropertyInfoAdapter,
+            ),
+            eventLogAdapter = EventLog.Adapter(
+                payloadAdapter = backInTimeDebugServiceEventAdapter,
             ),
         )
     }
