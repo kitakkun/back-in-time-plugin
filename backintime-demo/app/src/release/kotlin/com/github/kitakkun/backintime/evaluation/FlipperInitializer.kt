6e3b5735090bc6bc19c84ff4baac33e85af75eee
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-04-11 21:34:38 +0900

    delete FlipperInitializer for release variant

--- backintime-demo/app/src/release/kotlin/com/github/kitakkun/backintime/evaluation/FlipperInitializer.kt
+++
@@ -1,9 +0,0 @@
-package com.github.kitakkun.backintime.evaluation
-
-import android.content.Context
-
-object FlipperInitializer {
-    fun initFlipper(context: Context) {
-        // do nothing
-    }
-}
