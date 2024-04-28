151ab6c5e439b031b95e83b4e648b6556db667d0
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-04-28 15:46:51 +0900

    remove meaningless factories

--- backintime-debugger/app/src/commonMain/kotlin/com/github/kitakkun/backintime/debugger/AppModule.kt
+++ backintime-debugger/app/src/commonMain/kotlin/com/github/kitakkun/backintime/debugger/AppModule.kt
@@ -1,8 +1,6 @@
 package com.github.kitakkun.backintime.debugger
 
-import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val appModule = module {
-    factoryOf(::RootViewModel)
 }
