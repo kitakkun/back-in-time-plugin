0528230404d6329b4f3e4ee0a1151ce907f80697
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-05-04 00:21:58 +0900

    remove kind from EventLogRepository because events can be distinguished by their types
    also change entity id to auto-incremented integer value

--- backintime-debugger/data/src/commonMain/kotlin/com/github/kitakkun/backintime/debugger/data/repository/EventLogRepository.kt
+++ backintime-debugger/data/src/commonMain/kotlin/com/github/kitakkun/backintime/debugger/data/repository/EventLogRepository.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import kotlinx.datetime.Clock
 import kotlinx.serialization.encodeToString
-import java.util.UUID
 
 interface EventLogRepository {
     fun logFlow(sessionId: String): Flow<List<EventLog>>
@@ -27,9 +26,7 @@ class EventLogRepositoryImpl(private val queries: EventLogQueries) : CoroutineSc
     override suspend fun insert(sessionId: String, event: BackInTimeDebugServiceEvent) {
         withContext(coroutineContext) {
             queries.insert(
-                id = UUID.randomUUID().toString(),
                 sessionId = sessionId,
-                kind = event.javaClass.name,
                 payload = event,
                 createdAt = Clock.System.now().epochSeconds,
             )
