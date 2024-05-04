e68c22369a99d3038d94ad017452d505db029e48
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-05-04 23:08:58 +0900

    remove unused class ValueChangeInfo

--- backintime-debugger/data/src/commonMain/kotlin/com/github/kitakkun/backintime/debugger/data/model/ValueChangeInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.github.kitakkun.backintime.debugger.data.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ValueChangeInfo(
-    val propertyName: String,
-    val value: String,
-)
