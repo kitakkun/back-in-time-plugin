256feb60f38983a24eac2aebb7a7a7349ff76461
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-06-08 15:43:20 +0900

    remove needless auto-service configuration from gradle-plugin

--- backintime-plugin/gradle/build.gradle.kts
+++ backintime-plugin/gradle/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
-    alias(libs.plugins.ksp)
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.backintimeLint)
     `java-gradle-plugin`
@@ -28,8 +27,5 @@ dependencies {
     implementation(project(":backintime-plugin:common"))
     implementation(libs.kotlin.gradle.plugin.api)
     implementation(libs.kotlinx.serialization.json)
-
-    compileOnly(libs.auto.service)
-    ksp(libs.auto.service.ksp)
     compileOnly(kotlin("gradle-plugin"))
 }
