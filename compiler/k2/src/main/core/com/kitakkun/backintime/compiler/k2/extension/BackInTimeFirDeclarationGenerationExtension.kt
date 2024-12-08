40d8e14d573d7e7a93ac60fd5c2f7eaa5dbdea0e
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-12-08 21:49:00 +0900

    Fix the error when calling getSuperClassSymbolOrAny in Kotlin 2.0.20 or above

--- compiler/k2/src/main/core/com/kitakkun/backintime/compiler/k2/extension/BackInTimeFirDeclarationGenerationExtension.kt
+++ compiler/k2/src/main/core/com/kitakkun/backintime/compiler/k2/extension/BackInTimeFirDeclarationGenerationExtension.kt
@@ -11,17 +11,30 @@ import org.jetbrains.kotlin.fir.extensions.MemberGenerationContext
 import org.jetbrains.kotlin.fir.extensions.predicateBasedProvider
 import org.jetbrains.kotlin.fir.plugin.createMemberFunction
 import org.jetbrains.kotlin.fir.plugin.createMemberProperty
+import org.jetbrains.kotlin.fir.resolve.defaultType
 import org.jetbrains.kotlin.fir.symbols.impl.FirClassSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
 import org.jetbrains.kotlin.fir.types.classId
+import org.jetbrains.kotlin.fir.types.isSubtypeOf
 import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.Name
 
 class BackInTimeFirDeclarationGenerationExtension(session: FirSession) : FirDeclarationGenerationExtension(session) {
     override fun generateProperties(callableId: CallableId, context: MemberGenerationContext?): List<FirPropertySymbol> {
         val ownerClass = context?.owner ?: return emptyList()
+
+        // if it already has a property from the parent class, don't generate it.
+        if (
+            ownerClass.resolvedSuperTypes.any {
+                it.classId != BackInTimeConsts.backInTimeDebuggableInterfaceClassId && it.isSubtypeOf(
+                    superType = BackInTimeConsts.backInTimeDebuggableInterfaceClassId.defaultType(emptyList()),
+                    session = session,
+                )
+            }
+        ) {
+            return emptyList()
+        }
 
         val backInTimeDebuggableInterfaceType = ownerClass.resolvedSuperTypes.find { it.classId == BackInTimeConsts.backInTimeDebuggableInterfaceClassId } ?: return emptyList()
         val backInTimeDebuggableInterfaceClassSymbol = VersionSpecificAPI.INSTANCE.resolveToRegularClassSymbol(backInTimeDebuggableInterfaceType, session) ?: return emptyList()
@@ -72,27 +85,15 @@ class BackInTimeFirDeclarationGenerationExtension(session: FirSession) : FirDecl
     }
 
     override fun getCallableNamesForClass(classSymbol: FirClassSymbol<*>, context: MemberGenerationContext): Set<Name> {
-        return when {
-            classSymbol !is FirRegularClassSymbol -> emptySet()
-
-            // FIXME: need this to support class inheritance but it causes FirResolvedType error on 2.1.0
-//            classSymbol.getSuperClassSymbolOrAny(session).getSuperTypes(session).any { it.classId == BackInTimeConsts.backInTimeDebuggableInterfaceClassId } -> setOf(
-//                BackInTimeConsts.serializeMethodName,
-//                BackInTimeConsts.deserializeMethodName,
-//                BackInTimeConsts.forceSetValueMethodName,
-//            )
-
-            session.predicateBasedProvider.matches(BackInTimePredicate, classSymbol) -> setOf(
-                // methods
-                BackInTimeConsts.serializeMethodName,
-                BackInTimeConsts.deserializeMethodName,
-                BackInTimeConsts.forceSetValueMethodName,
-                // properties
-                BackInTimeConsts.backInTimeInstanceUUIDName,
-                BackInTimeConsts.backInTimeInitializedPropertyMapName,
-            )
-
-            else -> emptySet()
-        }
+        if (!session.predicateBasedProvider.matches(BackInTimePredicate, classSymbol)) return emptySet()
+        return setOf(
+            // methods
+            BackInTimeConsts.serializeMethodName,
+            BackInTimeConsts.deserializeMethodName,
+            BackInTimeConsts.forceSetValueMethodName,
+            // properties
+            BackInTimeConsts.backInTimeInstanceUUIDName,
+            BackInTimeConsts.backInTimeInitializedPropertyMapName,
+        )
     }
 }
