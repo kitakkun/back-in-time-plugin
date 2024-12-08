f75fe177d1d361fd9ae17a605e8867210d6b1ad4
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-12-08 16:05:10 +0900

    Migrate to gradlescript-based convention plugins

---
+++ gradle-conventions-settings/src/main/kotlin/util/Project.kt
@@ -0,0 +1,7 @@
+package util
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.getByType
+
+val Project.libs get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
--- gradle-conventions/build.gradle.kts
+++ gradle-conventions/build.gradle.kts
@@ -1,26 +1,9 @@
 plugins {
     `kotlin-dsl`
-    alias(libs.plugins.buildconfig)
-}
-
-gradlePlugin {
-    plugins {
-        register("com.kitakkun.backintime.conventions.ktlint") {
-            id = "backintime.lint"
-            implementationClass = "com.kitakkun.backintime.convention.LintConventionPlugin"
-        }
-        register("com.kitakkun.backintime.conventions.publication") {
-            id = "backintime.publication"
-            implementationClass = "com.kitakkun.backintime.convention.BackInTimePublicationPlugin"
-        }
-    }
 }
 
 dependencies {
+    implementation(":gradle-conventions-settings")
     compileOnly(libs.ktlint.gradle)
     compileOnly(libs.maven.publish)
-}
-
-buildConfig {
-    buildConfigField("VERSION", libs.versions.backintime.get())
 }
