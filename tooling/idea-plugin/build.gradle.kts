e91ef81b2fa76b0657003eb8d753cdabe5a08d1c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-01-08 20:44:34 +0900

    Setup empty compose ToolWindow by jewel

--- tooling/idea-plugin/build.gradle.kts
+++ tooling/idea-plugin/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.intelliJPlatform)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
@@ -12,10 +14,17 @@ repositories {
         defaultRepositories()
     }
     mavenCentral()
+    google()
+    maven("https://packages.jetbrains.team/maven/p/kpm/public/")
 }
 
 dependencies {
     intellijPlatform {
         create("IC", "2024.3.1")
+    }
+
+    implementation(libs.jewel)
+    implementation(compose.desktop.currentOs) {
+        exclude(group = "org.jetbrains.compose.material")
     }
 }
