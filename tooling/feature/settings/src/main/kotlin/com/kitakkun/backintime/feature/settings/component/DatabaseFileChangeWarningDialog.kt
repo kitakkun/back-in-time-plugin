b1a32bc34168c1875995bf81df7492aa2a3f9b9c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-02-11 17:13:53 +0900

    Commonize Dialog Implementation

--- tooling/feature/settings/src/main/kotlin/com/kitakkun/backintime/feature/settings/component/DatabaseFileChangeWarningDialog.kt
+++ tooling/feature/settings/src/main/kotlin/com/kitakkun/backintime/feature/settings/component/DatabaseFileChangeWarningDialog.kt
@@ -1,10 +1,8 @@
 package com.kitakkun.backintime.feature.settings.component
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,12 +14,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
+import com.kitakkun.backintime.tooling.core.ui.component.CommonConfirmationDialog
 import com.kitakkun.backintime.tooling.core.ui.preview.PreviewContainer
 import org.jetbrains.jewel.foundation.theme.JewelTheme
-import org.jetbrains.jewel.ui.component.ActionButton
 import org.jetbrains.jewel.ui.component.Checkbox
-import org.jetbrains.jewel.ui.component.DefaultButton
 import org.jetbrains.jewel.ui.component.Text
 
 @Composable
@@ -33,51 +29,33 @@ fun DatabaseFileChangeWarningDialog(
 ) {
     var migrateDataToInMemoryDatabase by remember { mutableStateOf(false) }
 
-    Dialog(onDismissRequest = onDismissRequest) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(16.dp),
+    CommonConfirmationDialog(
+        onDismissRequest = onDismissRequest,
+        onClickOk = { onClickOk(migrateDataToInMemoryDatabase) },
+        onClickCancel = onClickCancel,
+    ) {
+        Text(text = "The following file is being in use to handle debugger events:")
+        Text(
+            text = databaseFilePath,
             modifier = Modifier
-                .background(
-                    color = JewelTheme.globalColors.panelBackground,
-                    shape = RoundedCornerShape(32.dp),
+                .align(Alignment.CenterHorizontally)
+                .border(
+                    width = 1.dp,
+                    color = JewelTheme.globalColors.outlines.focusedWarning,
+                    shape = RoundedCornerShape(8.dp),
                 )
-                .padding(32.dp)
+                .padding(8.dp),
+        )
+        Text(text = "Are you sure to switching to In-Memory database?")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            Text(text = "The following file is being in use to handle debugger events:")
-            Text(
-                text = databaseFilePath,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .border(
-                        width = 1.dp,
-                        color = JewelTheme.globalColors.outlines.focusedWarning,
-                        shape = RoundedCornerShape(8.dp),
-                    )
-                    .padding(8.dp),
+            Checkbox(
+                checked = migrateDataToInMemoryDatabase,
+                onCheckedChange = { migrateDataToInMemoryDatabase = it },
             )
-            Text(text = "Are you sure to switching to In-Memory database?")
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-                Checkbox(
-                    checked = migrateDataToInMemoryDatabase,
-                    onCheckedChange = { migrateDataToInMemoryDatabase = it },
-                )
-                Text(text = "Migrate all events data to In-Memory database.(This will not delete current database file)")
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.align(Alignment.End),
-            ) {
-                ActionButton(onClickCancel) {
-                    Text(text = "Cancel")
-                }
-                DefaultButton(onClick = { onClickOk(migrateDataToInMemoryDatabase) }) {
-                    Text(text = "OK")
-                }
-            }
+            Text(text = "Migrate all events data to In-Memory database.(This will not delete current database file)")
         }
     }
 }
--- tooling/feature/settings/src/main/kotlin/com/kitakkun/backintime/feature/settings/component/DatabaseRecrationConfirmDialog.kt
+++ tooling/feature/settings/src/main/kotlin/com/kitakkun/backintime/feature/settings/component/DatabaseRecrationConfirmDialog.kt
@@ -1,14 +1,9 @@
 package com.kitakkun.backintime.feature.settings.component
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,13 +11,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
+import com.kitakkun.backintime.tooling.core.ui.component.CommonConfirmationDialog
 import com.kitakkun.backintime.tooling.core.ui.preview.PreviewContainer
-import org.jetbrains.jewel.foundation.theme.JewelTheme
-import org.jetbrains.jewel.ui.component.ActionButton
 import org.jetbrains.jewel.ui.component.Checkbox
-import org.jetbrains.jewel.ui.component.DefaultButton
 import org.jetbrains.jewel.ui.component.Text
 
 @Composable
@@ -32,45 +23,27 @@ fun DatabaseRecreationConfirmDialog(
 ) {
     var migrateCurrentData by remember { mutableStateOf(false) }
 
-    Dialog(onDismissRequest = onDismissRequest) {
-        Column(
-            modifier = Modifier
-                .background(
-                    shape = RoundedCornerShape(8.dp),
-                    color = JewelTheme.globalColors.panelBackground,
-                )
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+    CommonConfirmationDialog(
+        onDismissRequest = onDismissRequest,
+        onClickOk = { onClickApply(migrateCurrentData) },
+        onClickCancel = onDismissRequest,
+    ) {
+        Text("Database file location has been changed. Do you want to restart database?")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Text("Database file location has been changed. Do you want to restart database?")
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Checkbox(
-                    checked = migrateCurrentData,
-                    onCheckedChange = { migrateCurrentData = it },
+            Checkbox(
+                checked = migrateCurrentData,
+                onCheckedChange = { migrateCurrentData = it },
+            )
+            Text(
+                text = "Migrate current data to new database file.",
+                modifier = Modifier.clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = { migrateCurrentData = !migrateCurrentData },
                 )
-                Text(
-                    text = "Migrate current data to new database file.",
-                    modifier = Modifier.clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null,
-                        onClick = { migrateCurrentData = !migrateCurrentData },
-                    )
-                )
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.align(Alignment.End),
-            ) {
-                ActionButton(onClick = onDismissRequest) {
-                    Text("Cancel")
-                }
-                DefaultButton(onClick = { onClickApply(migrateCurrentData) }) {
-                    Text("Apply")
-                }
-            }
+            )
         }
     }
 }
