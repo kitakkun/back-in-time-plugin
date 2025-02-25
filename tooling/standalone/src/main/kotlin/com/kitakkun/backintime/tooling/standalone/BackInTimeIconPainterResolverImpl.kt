2fd625d40415fb2e5c4422b5a95e2008aaa711a7
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-02-25 12:00:52 +0900

    Replace with AutoMirrored Icon

--- tooling/standalone/src/main/kotlin/com/kitakkun/backintime/tooling/standalone/BackInTimeIconPainterResolverImpl.kt
+++ tooling/standalone/src/main/kotlin/com/kitakkun/backintime/tooling/standalone/BackInTimeIconPainterResolverImpl.kt
@@ -1,14 +1,14 @@
 package com.kitakkun.backintime.tooling.standalone
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.BugReport
-import androidx.compose.material.icons.filled.History
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Cloud
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Folder
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
@@ -24,7 +24,7 @@ class BackInTimeIconPainterResolverImpl : BackInTimeIconPainterResolver {
                 BackInTimeIconsKey.ToolWindowHierarchy -> Icons.Default.BugReport
                 BackInTimeIconsKey.DataSchema -> Icons.Default.History
                 BackInTimeIconsKey.ArrowDown -> Icons.Default.KeyboardArrowDown
-                BackInTimeIconsKey.ArrowRight -> Icons.Default.KeyboardArrowRight
+                BackInTimeIconsKey.ArrowRight -> Icons.AutoMirrored.Filled.KeyboardArrowRight
                 BackInTimeIconsKey.WebSocket -> Icons.Default.Cloud
                 BackInTimeIconsKey.EditSource -> Icons.Default.Edit
                 BackInTimeIconsKey.UiForm -> Icons.Default.Folder
