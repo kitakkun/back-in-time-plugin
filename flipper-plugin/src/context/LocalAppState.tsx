7334b5c5af2f2f46e60d932e4f23cfe38115eecf
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-04-20 14:39:58 +0900

    Remove flipper plugin

--- flipper-plugin/src/context/LocalAppState.tsx
+++
@@ -1,9 +0,0 @@
-import {createContext, useContext} from "react";
-import {com} from "backintime-flipper-lib";
-import FlipperAppState = com.kitakkun.backintime.tooling.flipper.FlipperAppState;
-
-export const LocalAppState = createContext<FlipperAppState>(FlipperAppState.Companion.Default)
-
-export function useAppState(): FlipperAppState {
-  return useContext(LocalAppState)
-}
--- flipper-plugin/src/context/StateOwnerContext.tsx
+++
@@ -1,9 +0,0 @@
-import {createContext, useContext} from "react";
-import {com} from "backintime-flipper-lib";
-import FlipperAppStateOwner = com.kitakkun.backintime.tooling.flipper.FlipperAppStateOwner;
-
-export const LocalStateOwner = createContext<FlipperAppStateOwner | null>(null)
-
-export function useStateOwner(): FlipperAppStateOwner {
-  return useContext(LocalStateOwner)!
-}
