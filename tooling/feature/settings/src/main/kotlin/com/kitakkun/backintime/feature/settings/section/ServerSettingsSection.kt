b57153881fb5b5e3d3c513938f683e33ab3f5c9a
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-04-29 11:26:46 +0900

    Change button text per state

--- tooling/feature/settings/src/main/kotlin/com/kitakkun/backintime/feature/settings/section/ServerSettingsSection.kt
+++ tooling/feature/settings/src/main/kotlin/com/kitakkun/backintime/feature/settings/section/ServerSettingsSection.kt
@@ -29,6 +29,7 @@ import kotlinx.coroutines.flow.filterNotNull
 import org.jetbrains.jewel.foundation.theme.JewelTheme
 import org.jetbrains.jewel.foundation.theme.LocalTextStyle
 import org.jetbrains.jewel.ui.component.ActionButton
+import org.jetbrains.jewel.ui.component.CircularProgressIndicator
 import org.jetbrains.jewel.ui.component.Text
 import org.jetbrains.jewel.ui.component.TextField
 
@@ -100,12 +101,39 @@ fun ServerSettingsSection(
                             text = "Port must be in range 0 to 65535.",
                             color = JewelTheme.globalColors.text.error,
                         )
-                    }
-                    if (needsServerRestart && !invalidPortRange) {
-                        ActionButton(
-                            content = { Text(text = "Apply") },
-                            onClick = onClickApply,
-                        )
+                    } else {
+                        when (status) {
+                            is SettingsScreenUiState.ServerStatus.Error -> {
+                                ActionButton(
+                                    onClick = onClickApply,
+                                ) {
+                                    Text(text = "Retry")
+                                }
+                            }
+
+                            is SettingsScreenUiState.ServerStatus.Started -> {
+                                if (needsServerRestart) {
+                                    ActionButton(
+                                        onClick = onClickApply,
+                                    ) {
+                                        Text(text = "Restart")
+                                    }
+                                }
+                            }
+
+                            SettingsScreenUiState.ServerStatus.Stopped -> {
+                                ActionButton(
+                                    onClick = onClickApply,
+                                ) {
+                                    Text(text = "Start")
+                                }
+                            }
+
+                            SettingsScreenUiState.ServerStatus.Starting,
+                            SettingsScreenUiState.ServerStatus.Stopping -> {
+                                CircularProgressIndicator()
+                            }
+                        }
                     }
                     TextField(
                         state = portNumberTextFieldState,
