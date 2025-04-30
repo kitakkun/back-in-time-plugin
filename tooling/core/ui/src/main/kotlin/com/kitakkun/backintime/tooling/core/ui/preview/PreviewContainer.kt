ca32704ff87af1150e7df2e6fda45cef07b27f30
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-04-30 12:54:03 +0900

    Remove outdated comment

--- tooling/core/ui/src/main/kotlin/com/kitakkun/backintime/tooling/core/ui/preview/PreviewContainer.kt
+++ tooling/core/ui/src/main/kotlin/com/kitakkun/backintime/tooling/core/ui/preview/PreviewContainer.kt
@@ -9,9 +9,6 @@ import com.kitakkun.backintime.tooling.core.ui.theme.LocalIsIDEInDarkTheme
 import org.jetbrains.jewel.foundation.theme.JewelTheme
 import org.jetbrains.jewel.intui.standalone.theme.IntUiTheme
 
-// FIXME: Can't preview due to kotlinx/coroutines/Dispatchers missing
-//  We have to add kotlinx-coroutines dependency by implementation but it causes the runtime error on IDEA Plugin.
-//  See intellij-common.gradle.kts for details.
 @Composable
 fun PreviewContainer(
     content: @Composable () -> Unit,
