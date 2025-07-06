42f239bf63ae081bc08bb3f5130d7524c3b01472
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-07-06 20:39:47 +0900

    Upgrade Kotlin to 2.2.0

--- gradle-conventions-settings/build.gradle.kts
+++ gradle-conventions-settings/build.gradle.kts
@@ -2,12 +2,6 @@ plugins {
     alias(libs.plugins.gradleKotlinDsl)
 }
 
-configurations.configureEach {
-    resolutionStrategy {
-        force(libs.kotlin.stdlib)
-    }
-}
-
 dependencies {
     compileOnly(libs.jetbrains.compose.gradle.plugin)
 }
